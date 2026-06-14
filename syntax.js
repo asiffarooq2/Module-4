86b9db7188281fcbf264c48ae45dae063fcaead0
Asif <asiffarooq2@gmail.com>
2026-06-14 08:24:39 +0530

    initial

---
+++ syntax.js
@@ -0,0 +1,21 @@
+// JavaScript Syntax and Comments Example
+
+// Single-line comment
+
+/*
+   Multi-line comment
+   This comment can span
+   multiple lines.
+*/
+
+// Variable declaration
+let name = "Asif";
+let course = "JavaScript";
+
+// Display output
+console.log("Name:", name);
+console.log("Course:", course);
+
+// Simple statement
+let age = 25;
+console.log("Age:", age);
